07096b6c05dee08e7532d3e247307ba7bf3e2f3e
white-phb <pehhongbo@1utar.my>
2023-10-14 23:24:21 +0800

    update contract to store approved node

--- smartContract/contracts/PeerConnectionRequest.sol
+++ smartContract/contracts/PeerConnectionRequest.sol
@@ -2,20 +2,28 @@
 pragma solidity ^0.8.0;
 
 contract PeerConnectionRequest {
-    address public owner;
-    mapping(address => bool) public peerRequests;
+    address public owner; // Contract owner or administrator
+    mapping(address => bool) public peerRequests; // Mapping to track peer connection requests
+    address[] public approvedPeers; // List of approved peers
 
     constructor() {
-        owner = msg.sender;
+        owner = msg.sender; // Set the contract owner as the deployer
     }
 
     function submitPeerRequest() public {
         require(msg.sender != owner, "Contract owner cannot submit a request.");
-        peerRequests[msg.sender] = true;
+        peerRequests[msg.sender] = true; // Mark the sender as a requester
     }
 
     function approvePeerRequest(address requester) public {
         require(msg.sender == owner, "Only the owner can approve requests.");
-        peerRequests[requester] = false;
+        require(peerRequests[requester], "Requester has not submitted a request.");
+        peerRequests[requester] = false; // Mark the request as approved
+        approvedPeers.push(requester); // Add the requester to the list of approved peers
     }
+
+    function getApprovedPeers() public view returns (address[] memory) {
+        return approvedPeers;
+    }
+
 }
